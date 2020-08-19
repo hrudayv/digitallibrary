e38fca0a3f8d93c0665e3384c206dcc55fa7a322
hrudayv <45425783+hrudayv@users.noreply.github.com>
2020-08-20 00:56:24 +0530

    Update settings.py

--- digitallibrary/digitallibrary/settings.py
+++ digitallibrary/digitallibrary/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vze5m$bz#=eq#$b2=tf4af$&%b^$szzean-p0f4=*#r$*e(y_$'
+# THE SECRET KEY IS REMOVED
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
